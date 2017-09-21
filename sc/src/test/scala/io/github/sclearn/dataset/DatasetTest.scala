5af9a99840f27bc9b74c0be55f6d45841f999450
Vasilii Pankratov <pankratov.vs@gmail.com>
2017-09-21 23:25:30 +0500

    upgrade junit version to 5th

---
+++ sc/src/test/scala/io/github/sclearn/dataset/DatasetTest.scala
@@ -0,0 +1,6 @@
+package io.github.sclearn.dataset
+
+
+class DatasetTest {
+  
+}
--- sc/src/test/scala/io/github/sclearn/sklearn/linear_model/LinearRegressionTests.scala
+++ sc/src/test/scala/io/github/sclearn/sklearn/linear_model/LinearRegressionTests.scala
@@ -3,15 +3,14 @@ package sklearn.linear_model
 
 import org.scalatest.junit.AssertionsForJUnit
 import scala.collection.mutable.ListBuffer
-import org.junit.Assert._
-import org.junit.Test
-import org.junit.Before
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
 
-
-class LinearRegressionTest extends AssertionsForJUnit {
+class LinearRegressionTests {
 
 	// TODO: implement all
 	@Test
+  @DisplayName("😱")
 	def testLinearRegression = {
 
 		val train_X	= null
@@ -19,6 +18,7 @@ class LinearRegressionTest extends AssertionsForJUnit {
 		val test_X	= null
 		val test_y	= null
 
+		assert(1 == 1)
 //		val regr = LinearRegression()
 //		regr.fit(train_X, train_y)
 //
