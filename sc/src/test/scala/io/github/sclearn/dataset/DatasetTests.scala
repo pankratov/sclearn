42e5743522a46d1746f99670849a9ee238c145ba
Vasilii Pankratov <pankratov.vs@gmail.com>
2017-09-27 03:22:33 +0500

    breeze library into the project

--- sc/src/test/scala/io/github/sclearn/dataset/DatasetTests.scala
+++ sc/src/test/scala/io/github/sclearn/dataset/DatasetTests.scala
@@ -3,24 +3,52 @@ package io.github.sclearn.dataset
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
 import io.github.sclearn.Dataset
+import org.junit.runner.RunWith
+import org.junit.platform.runner.JUnitPlatform
+import breeze.linalg.DenseVector
+import breeze.linalg.DenseMatrix
 
+@RunWith(classOf[JUnitPlatform])
 class DatasetTests {
+
   implicit class MatrixHelper(val sc: StringContext) {
-    def m(args: Any*): Array[Array[Double]] = {
-      sc.parts.mkString
-        .replaceAll("[\\[\\]]", "")
-        .split(";")
-        .map(_.split("\\s+")
-          .filter(_.matches("\\d+(\\.\\d+)?"))
-          .map(_.toDouble))
+    private def parsePart(str: String): List[Double] = {
+      str.replaceAll("[\\[\\];]", " ")
+        .trim().split("\\s+")
+        .map(_.toDouble).toList
+    }
+
+    private def parseValues(args: Double*): List[Double] = {
+      parsePart(sc.parts.head) ++ args.zip(sc.parts.tail).flatMap {
+        case (arg, part) => arg :: parsePart(part)
+      }
+    }
+
+    def m(args: Double*): DenseMatrix[Double] = {
+      val values = parseValues(args: _*)
+
+      val concatenated = sc.parts.mkString("0")
+      val rowStr = concatenated.takeWhile(_ != ';')
+      val colNum = rowStr.tail.trim().split("\\s+").length
+      val rowNum = values.length / colNum
+
+      new DenseMatrix(colNum, rowNum, values.toArray).t
+    }
+
+    def v(args: Double*): DenseVector[Double] = {
+      val values = parseValues(args: _*)
+      new DenseVector(values.toArray)
     }
   }
 
   @Test
   def testSomething {
-    val x = 3
-    val matrix = m"[1 2 3; 4 5 6]"
-
+    val y = 2
+    val matrix = m"[1 $y; 3 4; 5 6]"
+    val vector = v"[1 $y 3 4 5 6]"
+    println(matrix)
+    println(vector)
+    
     assertEquals(1, 1)
   }
 
